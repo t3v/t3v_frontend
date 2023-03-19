892f460d1acd30d1a12551727f06ce7112159480
Maik Kempe <mkempe@bitaculous.com>
2023-03-19 18:34:28 +0100

    [➠] Updated rendering test.

--- Tests/Functional/Frontend/RenderingTest.php
+++ Tests/Functional/Frontend/RenderingTest.php
@@ -39,9 +39,9 @@ class RenderingTest extends FunctionalTestCase
      * @var array
      */
     protected $testExtensionsToLoad = [
-        'typo3conf/ext/t3v_frontend',
         'typo3conf/ext/t3v_core',
-        'typo3conf/ext/t3v_testing'
+        'typo3conf/ext/t3v_testing',
+        'typo3conf/ext/t3v_frontend'
     ];
 
     /**
